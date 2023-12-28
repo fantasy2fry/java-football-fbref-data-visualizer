14545d09779e68e4331ec476fae9d2b8bd7086ca
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2023-12-28 13:57:16 +0100

    players Stats parser finished add

--- src/data/collect/PlayersStatsGetter.java
+++ src/data/collect/PlayersStatsGetter.java
@@ -10,23 +10,45 @@ import tech.tablesaw.columns.Column;
 import tech.tablesaw.io.DataFrameReader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class PlayersStatsGetter {
+    private Map<String, String> clubsUrls=new HashMap<>();
 
     public static void main(String[] args) {
-        String websiteUrl = "https://fbref.com/en/squads/18bb7c10/2023-2024/all_comps/Arsenal-Stats-All-Competitions#all_stats_standard"; // Replace with the website URL you want to scrape
-        Table table;
-        List<List<String>> tableData = playersStats(websiteUrl);
-        System.out.println(tableData);
-        table=createTableFromListByColumns(tableData, "Players Stats");
+        PlayersStatsGetter playersStatsGetter=new PlayersStatsGetter();
+        Table table=playersStatsGetter.getPlayersStats("Arsenal");
+        System.out.println(table);
+        table=playersStatsGetter.getPlayersStats("Barcelona");
         System.out.println(table);
 
+//        String websiteUrl = "https://fbref.com/en/squads/18bb7c10/2023-2024/all_comps/Arsenal-Stats-All-Competitions#all_stats_standard"; // Replace with the website URL you want to scrape
+//        Table table;
+//        List<List<String>> tableData = playersStats(websiteUrl);
+//        System.out.println(tableData);
+//        table=createTableFromListByColumns(tableData, "Players Stats");
+//        System.out.println(table);
+//        System.out.println("-------Transpose-------");
+//        table=table.transpose();
+//        System.out.println(table);
+//        table=createTableFromListByRows(tableData, "Players Stats");
+//        System.out.println(table);
+//        System.out.println("-------Transpose-------");
+//        table=table.transpose();
+//        System.out.println(table);
         //System.out.println(playersStats("https://fbref.com/en/comps/9/stats/Premier-League-Stats", 0));
+    }
+    public PlayersStatsGetter(){
+        clubsUrls.put("Arsenal","https://fbref.com/en/squads/18bb7c10/2023-2024/all_comps/Arsenal-Stats-All-Competitions#all_stats_standard");
+        clubsUrls.put("Barcelona", "https://fbref.com/en/squads/206d90db/2023-2024/all_comps/Barcelona-Stats-All-Competitions");
+    }
+
+    public Table getPlayersStats(String clubName){
+        String websiteUrl=clubsUrls.get(clubName);
+        List<List<String>> tableData = playersStats(websiteUrl);
+        Table table=createTableFromListByRows(tableData, clubName);
+        return table;
     }
 
     private static List<List<String>> extractTableData(Element table) {
@@ -87,6 +109,21 @@ public class PlayersStatsGetter {
         table = Table.create(tableName);
         // Add the StringColumns to the table
         for (StringColumn stringColumn : stringColumns) {
+            table.addColumns(stringColumn);
+        }
+        return table;
+    }
+    private static Table createTableFromListByRows(List<List<String>> data, String tableName) {
+        int columns =data.get(1).size();
+        int rows= data.size();
+        Table table=Table.create(tableName);
+        for (int i=0;i<columns;i++){
+            List<String> list=new ArrayList<>();
+            String columnName=((Integer)i).toString()+ " "+data.get(1).get(i);
+            for (int j=2;j<rows;j++){
+                list.add(data.get(j).get(i));
+            }
+            StringColumn stringColumn = StringColumn.create(columnName, list);
             table.addColumns(stringColumn);
         }
         return table;
