41b5e8d6edb246056604825131eb3907cc7a0953
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2024-01-06 12:26:45 +0100

    more clubs added and import functions to get info to GUI added

--- src/data/collect/PlayersStatsGetter.java
+++ src/data/collect/PlayersStatsGetter.java
@@ -19,10 +19,13 @@ public class PlayersStatsGetter {
 
     public static void main(String[] args) {
         PlayersStatsGetter playersStatsGetter=new PlayersStatsGetter();
-        Table table=playersStatsGetter.getPlayersStats("Arsenal");
+        Table table=playersStatsGetter.getPlayersStats("Napoli");
         System.out.println(table);
-        table=playersStatsGetter.getPlayersStats("Barcelona");
+        table=playersStatsGetter.getPlayersStats("Real Madrid");
         System.out.println(table);
+        System.out.println(playersStatsGetter.getPlayersFromTable(table));
+        System.out.println(playersStatsGetter.getAllClubNames());
+        System.out.println(playersStatsGetter.getImportantColumnNamesFromTable(table));
 
 //        String websiteUrl = "https://fbref.com/en/squads/18bb7c10/2023-2024/all_comps/Arsenal-Stats-All-Competitions#all_stats_standard"; // Replace with the website URL you want to scrape
 //        Table table;
@@ -43,6 +46,22 @@ public class PlayersStatsGetter {
     public PlayersStatsGetter(){
         clubsUrls.put("Arsenal","https://fbref.com/en/squads/18bb7c10/2023-2024/all_comps/Arsenal-Stats-All-Competitions#all_stats_standard");
         clubsUrls.put("Barcelona", "https://fbref.com/en/squads/206d90db/2023-2024/all_comps/Barcelona-Stats-All-Competitions");
+        clubsUrls.put("Liverpool","https://fbref.com/en/squads/822bd0ba/2023-2024/all_comps/Liverpool-Stats-All-Competitions");
+        clubsUrls.put("Manchester City","https://fbref.com/en/squads/b8fd03ef/2023-2024/all_comps/Manchester-City-Stats-All-Competitions");
+        clubsUrls.put("Aston Villa", "https://fbref.com/en/squads/8602292d/2023-2024/all_comps/Aston-Villa-Stats-All-Competitions");
+        clubsUrls.put("Paris Saint-Germain","https://fbref.com/en/squads/e2d8892c/2023-2024/all_comps/Paris-Saint-Germain-Stats-All-Competitions");
+        clubsUrls.put("Bayern Munich", "https://fbref.com/en/squads/054efa67/2023-2024/all_comps/Bayern-Munich-Stats-All-Competitions");
+        clubsUrls.put("Bayer Leverkusen","https://fbref.com/en/squads/c7a9f859/2023-2024/all_comps/Bayer-Leverkusen-Stats-All-Competitions");
+        clubsUrls.put("Girona","https://fbref.com/en/squads/9024a00a/2023-2024/all_comps/Girona-Stats-All-Competitions");
+        clubsUrls.put("Real Madrid","https://fbref.com/en/squads/53a2f082/2023-2024/all_comps/Real-Madrid-Stats-All-Competitions");
+        clubsUrls.put("Manchester United","https://fbref.com/en/squads/19538871/2023-2024/all_comps/Manchester-United-Stats-All-Competitions");
+        clubsUrls.put("Nice","https://fbref.com/en/squads/132ebc33/2023-2024/all_comps/Nice-Stats-All-Competitions");
+        clubsUrls.put("Tottenham Hotspur","https://fbref.com/en/squads/361ca564/2023-2024/all_comps/Tottenham-Hotspur-Stats-All-Competitions");
+        clubsUrls.put("Chelsea","https://fbref.com/en/squads/cff3d9bb/2023-2024/all_comps/Chelsea-Stats-All-Competitions");
+        clubsUrls.put("Internazionale","https://fbref.com/en/squads/d609edc0/2023-2024/all_comps/Internazionale-Stats-All-Competitions");
+        clubsUrls.put("Juventus","https://fbref.com/en/squads/e0652b02/2023-2024/all_comps/Juventus-Stats-All-Competitions");
+        clubsUrls.put("Brighton","https://fbref.com/en/squads/d07537b9/2023-2024/all_comps/Brighton-and-Hove-Albion-Stats-All-Competitions");
+        clubsUrls.put("Napoli","https://fbref.com/en/squads/d48ad4ff/2023-2024/all_comps/Napoli-Stats-All-Competitions");
     }
 
     public Table getPlayersStats(String clubName){
@@ -128,6 +147,28 @@ public class PlayersStatsGetter {
             table.addColumns(stringColumn);
         }
         return table;
+    }
+
+    public List<String> getPlayersFromTable(Table table){
+        List<String> players=new ArrayList<>();
+        StringColumn stringColumn=table.stringColumn(0);
+        for (String player:stringColumn) {
+            players.add(player);
+        }
+        return players;
+    }
+    public List<String> getAllClubNames(){
+        List<String> clubNames=new ArrayList<>();
+        for (String clubName:clubsUrls.keySet()) {
+            clubNames.add(clubName);
+        }
+        return clubNames;
+    }
+    public List<String> getImportantColumnNamesFromTable(Table table){
+        // take 5th column na to 23rd column
+        List<String> columnNames=table.columnNames();
+        columnNames=columnNames.subList(4,23);
+        return columnNames;
     }
 }
 
